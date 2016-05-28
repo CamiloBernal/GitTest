55fd30e90134985a94707cc07bc276bf6a0c2bea
CamiloBernal <camilobernal@outlook.com>
2016-05-27 19:10:06 -0500

    Add second server

---
+++ server2.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const hostName = "localhost";
+const port = 8080;
+ 
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<!doctype html><html><head><title>My First Node Server 2</title></head><body><h1>Hello world 2 from nodejs</h1></body></html>');
+}).listen(port, hostName, () => {
+  console.log(`Server running at http://${hostName}:${port}/`);
+});
+ 
